97c478b3f207f10b8595b2b6fc438c6e20b85282
AlanLespron-saxum <alan.lespron@saxum-tech.com>
2023-05-29 03:29:39 -0600

    Cambios a Buffer

--- GUI/src/Buffer.java
+++ GUI/src/Buffer.java
@@ -1,50 +1,64 @@
 
-
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Buffer {
-    
-    private int buffer;
-    
-    Buffer(int bufferSize) {
-        this.buffer = 0;
+
+    private SchemeOp[] buffer;
+    public Random random;
+    public int waitCons, waitProd;
+
+    Buffer(int bufferSize, int waitProducer, int waitConsumer) {
+        this.buffer = new SchemeOp[bufferSize];
+        random = new Random(System.currentTimeMillis());
+        waitCons = waitConsumer;
+        waitProd = waitProducer;
     }
-    
-    synchronized int consume() {
-        int product = 0;
-        
-        if(this.buffer == 0) {
+
+    synchronized SchemeOp consume() {
+        SchemeOp product = null;
+        int notNullBuffer = -1;
+
+        for (int i = 0; i < this.buffer.length; i++) {
+            if (this.buffer[i] != null) {
+                notNullBuffer = i;
+                break;
+            }
             try {
-                wait(1000);
+                wait(waitCons);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Buffer.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        product = this.buffer;
-        this.buffer = 0;
-        notify();
-        
+        if (notNullBuffer < 0) {
+            return product;
+        }
+        product = this.buffer[notNullBuffer];
+        this.buffer[notNullBuffer] = null;
+        notifyAll();
         return product;
     }
-    
-    synchronized void produce(int product) {
-        if(this.buffer != 0) {
+
+    synchronized void produce(SchemeOp product) {
+        int randBufferNum = random.nextInt(0, this.buffer.length);
+        while (this.buffer[randBufferNum] != null) {
             try {
-                wait(1000);
+                wait(waitProd);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Buffer.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        this.buffer = product;
-        
-        notify();
+        this.buffer[randBufferNum] = product;
+
+        notifyAll();
     }
-    
+
     static int count = 1;
+
     synchronized static void print(String string) {
         System.out.print(count++ + " ");
         System.out.println(string);
     }
-    
+
 }
