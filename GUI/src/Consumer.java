8a33bf3b1530423555fdd45bd8371ffd8346036d
AlanLespron-saxum <alan.lespron@saxum-tech.com>
2023-05-29 12:02:35 -0600

    cambios clase consumer

--- GUI/src/Consumer.java
+++ GUI/src/Consumer.java
@@ -3,27 +3,27 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Consumer extends Thread {
+
     Buffer buffer;
-    
+
     Consumer(Buffer buffer) {
         this.buffer = buffer;
     }
-    
+
     @Override
     public void run() {
-        System.out.println("Running Consumer...");
-        int product;
-        
-        for(int i=0 ; i<5 ; i++) {
-            product = this.buffer.consume();
-            //System.out.println("Consumer consumed: " + product);
-            Buffer.print("Consumer consumed: " + product);
-            
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        SchemeOp product;
+        product = this.buffer.consume();
+        //System.out.println("Running Consumer...");
+        if (product == null) {
+            Buffer.print("Consumer couldn't find a not null buffer.");
+        } else {
+            Buffer.print("Consumer consumed: " + "Id -> " + product.Id + " Op -> " + product.SchemeFormat() + " Result -> " + product.result);
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
